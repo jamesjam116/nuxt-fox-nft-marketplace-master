d26502a02a21f532224fa39fd08d7ec4e2348435
A.Kunitaro <125228912+Aurora0116@users.noreply.github.com>
2023-03-24 03:44:18 +0900

    Update apiService.js

--- helpers/apiService.js
+++ helpers/apiService.js
@@ -3,3 +3,7 @@ import axios from 'axios'
 export const requestAPICall = async (url) => {
   return await axios.get(url)
 }
+
+export const requestAPICall1 = async (url) => {
+  return await axios.get(url)
+}
